849b6116a760189a7c5a29816378f115e46ebd6d
Weng Ti <wengti@hotmail.com>
2026-01-15 19:47:22 +0800

    Add hint

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,62 @@
-import { useState, useMemo } from "react"
+import { useState, useMemo, useEffect } from "react"
 import { clsx } from "clsx"
 import { languages } from "./languages"
 import { getFarewellText, getRandomWord } from "./utils"
-import Confetti from "react-confetti"
 import { useReward } from 'react-rewards'
 
 export default function AssemblyEndgame() {
 
-
+  const timeLimit = 90*1000
+  const hintPenalty = timeLimit * 1/3
 
   // State values
-  const [currentWord, setCurrentWord] = useState(() => getRandomWord())
+  const [currentWord, setCurrentWord] = useState(() => getRandomWord()) //() => getRandomWord()
   const [guessedLetters, setGuessedLetters] = useState([])
+  const [isHintAllowed, setIsHintAllowed] = useState(false)
+  const [isHintUsed, setIsHintUsed] = useState(false)
   const [timer, setTimer] = useState(null)
-  const [timeLeft, setTimeLeft] = useState(90 * 1000)
+  const [timeLeft, setTimeLeft] = useState(timeLimit)
 
   // Static values
-  const timeLimit = 90 * 1000 //1:30 mins = 90000ms
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
-  const timeLeftMin = String(timeLeft < 0 ? 0 : Math.floor((timeLeft / (60 * 1000)))).padStart(2, "0")
-  const timeLeftSec = String(timeLeft < 0 ? 0 : Math.floor((timeLeft % (60 * 1000)) / 1000)).padStart(2, "0")
+
   const { reward: lossConfetti, isAnimating: isLossConfettiAnimating } = useReward(
     "confetti-section", "emoji", { angle: 270, emoji: ['🗑️', '💀', '👎', '😠', '😞'], startVelocity: 15, spread: 180 })
   const { reward: winConfetti, isAnimating: isWinConfettiAnimating } = useReward(
     "confetti-section", "confetti", { angle: 270, startVelocity: 15, spread: 180 })
 
   // Derived values
+  const trueTimeLeft = timeLeft - (isHintUsed > 0 ? hintPenalty : 0) // Take into account whether the hint is used
+  const timeLeftMin = String(trueTimeLeft < 0 ? 0 : Math.floor((trueTimeLeft / (60 * 1000)))).padStart(2, "0")
+  const timeLeftSec = String(trueTimeLeft < 0 ? 0 : Math.floor((trueTimeLeft % (60 * 1000)) / 1000)).padStart(2, "0")
+
+
   const numGuessesLeft = languages.length - 1
   const wrongGuessCount =
     guessedLetters.filter(letter => !currentWord.includes(letter)).length
-  const isGameLost = (wrongGuessCount >= numGuessesLeft) || (timeLeft < 0)
+  const isGameLost = (wrongGuessCount >= numGuessesLeft) || (trueTimeLeft < 0)
   const isGameWon =
     (currentWord.split("").every(letter => guessedLetters.includes(letter))) && !isGameLost
   const isGameOver = isGameWon || isGameLost
   const lastGuessedLetter = guessedLetters[guessedLetters.length - 1]
   const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
 
-
-
   if (isGameOver && timer !== null) {
     clearInterval(timer)
     setTimer(null)
+    setIsHintAllowed(false)
   }
-
 
   function addGuessedLetter(letter) {
 
-
     if (timer === null) {
+
+      setIsHintUsed(false)
+      setIsHintAllowed(true)
+
       const now = new Date()
       setTimer(setInterval(() => {
-        setTimeLeft(timeLimit - (new Date() - now))
+        setTimeLeft(() => checkTimeLeft(now))
       }, 100))
     }
 
@@ -61,11 +67,29 @@ export default function AssemblyEndgame() {
     )
   }
 
+  function checkTimeLeft(now) {
+    const calcTimeLeft = timeLimit - (new Date() - now)
+    return calcTimeLeft
+  }
+
+  function handleHint() {
+    //Check if hint is used
+    if (!isHintUsed && isHintAllowed) {
+      setIsHintUsed(true)
+      setIsHintAllowed(false)
+      // Select the character to add
+      // call add guessed letter
+      addGuessedLetter(currentWord[Math.floor(Math.random() * (currentWord.length))])
+    }
+  }
+
   function startNewGame() {
-    setCurrentWord(getRandomWord())
+    setCurrentWord(getRandomWord()) //getRandomWord()
     setGuessedLetters([])
+    setIsHintUsed(false)
     setTimeLeft(timeLimit)
   }
+
 
   const languageElements = languages.map((lang, index) => {
     const isLanguageLost = index < wrongGuessCount
@@ -131,6 +155,7 @@ export default function AssemblyEndgame() {
     return renderGameStatus()
   }, [guessedLetters, isGameOver])
 
+
   function renderGameStatus() {
     if (!isGameOver && isLastGuessIncorrect) {
       return (
@@ -172,26 +197,31 @@ export default function AssemblyEndgame() {
   }
 
   const timeStyle = {
-    color: timeLeft < (timeLimit * 1 / 3) ? "#EC5D49" : "inherit"
+    color: trueTimeLeft < hintPenalty ? "#EC5D49" : "inherit"
   }
 
   //hsl(0, 0%, 15%) to hsl(0, 59%, 29%)
-  const timeRatio = (timeLimit - timeLeft) / timeLimit
-  const guessRatio = wrongGuessCount / (languages.length - 1)
-  const ratio = timeRatio >= guessRatio ? timeRatio : guessRatio
-  const hue = 0
-  const saturation = 59 * ratio
-  const light = 15 + (29 - 15) * ratio
-  const mainStyle = {
-    backgroundColor: `hsl(${hue}, ${saturation}%, ${light}%)`
+  let mainStyle = {}
+  if (!isGameWon) {
+    const timeRatio = (timeLimit - timeLeft) / timeLimit
+    const guessRatio = wrongGuessCount / (languages.length - 1)
+    const ratio = timeRatio >= guessRatio ? timeRatio : guessRatio
+    const hue = 0
+    const saturation = 59 * ratio
+    const light = 15 + (29 - 15) * ratio
+    mainStyle = {
+      backgroundColor: `hsl(${hue}, ${saturation}%, ${light}%)`
+    }
   }
+
+
 
   return (
     <main id='main' style={mainStyle}>
 
       <section id="confetti-section">
       </section>
- 
+
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word within 8 attempts and 90s to keep the
@@ -234,6 +264,12 @@ export default function AssemblyEndgame() {
           .join(" ")}</p>
 
       </section>
+
+      <button
+        className='hint-btn'
+        onClick={handleHint}
+        disabled={!isHintAllowed || trueTimeLeft < hintPenalty} // 
+      >{`Spend ${hintPenalty / 1000}s for a hint`}</button>
 
       <section className="keyboard">
         {keyboardElements}
---
+++ test.js
@@ -0,0 +1,11 @@
+
+
+let a = 1000
+
+setInterval( () => {
+    console.log(a)
+}, 1000)
+
+setInterval( () => {
+    a += 100
+}, 100)
